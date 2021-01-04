06c49a4250ea3945f5ff7fb5e96ab28728254066
Alla Rakhimova <alla.rakhimova103@gmail.com>
2021-01-05 01:11:46 +0200

    eslint

--- main.js
+++ main.js
@@ -1,34 +1,9 @@
-const link = `https://spreadsheets.google.com/feeds/cells/1PXorfz2O2NqH-FcW0nA-HhmtZMmSSwgHheifWc0e1tU/2/public/full?alt=json`;
-
-fetch(link)
-  .then(res => res.json())
-  .then(data => console.log(parseData(data.feed.entry)));
-
-
-function parseData(arr) {
-  const names = ['id', 'productName', 'manufacture', 'category', 'ingridients', 'amount', 'units', 'price', 'image'];
-  const shift = names.length;
-
-  return arr.reduce( (acc, {content}, i) => {
-    const index = Math.floor(i / shift) - 1;
-    const name = names[i % shift];
-
-    if ( index === -1 ) {
-      return acc;
-    }
-
-    if (!acc[index]) {
-      acc[index] = {}
-    }
-
-    acc[index][name] = parseContent(name, content.$t);
-
-    return acc;
-  }, []);
-}
+const link = 'https://spreadsheets.google.com/feeds/cells/1PXorfz2O2NqH-FcW0nA-HhmtZMmSSwgHheifWc0e1tU/2/public/full?alt=json';
 
 function parseContent(name, data) {
   let newVal;
+  let num;
+  let unit;
 
   switch (name) {
     case 'id':
@@ -37,8 +12,8 @@ function parseContent(name, data) {
       newVal = +(data.replace(',', '.').trim());
       break;
     case 'units':
-      const num = +(data.match(/([^a-z]+)/gi)[0].replace(',', '.').trim());
-      const unit = data.match(/([a-z]+)/gi)[0].trim();
+      num = +(data.match(/([^a-z]+)/gi)[0].replace(',', '.').trim());
+      unit = data.match(/([a-z]+)/gi)[0].trim();
       newVal = [num, unit];
       break;
     default:
@@ -47,3 +22,29 @@ function parseContent(name, data) {
 
   return newVal;
 }
+
+function parseData(arr) {
+  const names = ['id', 'productName', 'manufacture', 'category', 'ingridients', 'amount', 'units', 'price', 'image'];
+  const shift = names.length;
+
+  return arr.reduce((acc, { content }, i) => {
+    const index = Math.floor(i / shift) - 1;
+    const name = names[i % shift];
+
+    if (index === -1) {
+      return acc;
+    }
+
+    if (!acc[index]) {
+      acc[index] = {};
+    }
+
+    acc[index][name] = parseContent(name, content.$t);
+
+    return acc;
+  }, []);
+}
+
+fetch(link)
+  .then((res) => res.json())
+  .then((data) => console.log(parseData(data.feed.entry)));
